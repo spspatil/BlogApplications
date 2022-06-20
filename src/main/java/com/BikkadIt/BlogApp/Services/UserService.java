bae6ba0ca24388d0f5a3dde91a9068147b5693c1
shinusmp <shrinivaspatil12@gmail.com>
2022-06-20 23:06:09 +0530

    BLOG-06: created userservice Interface

--- src/main/java/com/BikkadIt/BlogApp/Services/UserService.java
+++ src/main/java/com/BikkadIt/BlogApp/Services/UserService.java
@@ -1,5 +1,16 @@
 package com.BikkadIt.BlogApp.Services;
 
+import java.util.List;
+
+import com.BikkadIt.BlogApp.Payloads.UserDTO;
+
 public interface UserService {
+	
+	UserDTO createUser(UserDTO user);
+	UserDTO updateUser(UserDTO user,Integer userId);
+	UserDTO getuserByid(Integer userId);
+	List<UserDTO> getAllUser();
+	
+	void deleteUser(Integer userId);
 
 }
