56b670f39cea2a12ec2cd1de25db18acf6169d13
shinusmp <shrinivaspatil12@gmail.com>
2022-06-21 00:45:34 +0530

    BLOG-17:created controller class

---
+++ src/main/java/com/BikkadIt/BlogApp/Controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.BlogApp.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	
+	
+	
+
+}
